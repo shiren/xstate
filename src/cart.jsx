254a94aa7c05d69597dd808f096acb3668ec78c2
Sungho Kim <shirenbeat@gmail.com>
2022-08-25 19:41:36 +0900

    액션 예제

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,20 +1,34 @@
 import { createMachine } from 'xstate';
 import { useMachine } from '@xstate/react';
 
-const stateMachine = createMachine({
-  id: 'cart',
-  initial: 'empty',
-  states: {
-    empty: {
-      on: {
-        ADD_ITEM: {
-          target: 'hold',
+const stateMachine = createMachine(
+  {
+    id: 'cart',
+    initial: 'empty',
+    context: {
+      items: [],
+    },
+    states: {
+      empty: {
+        on: {
+          ADD_ITEM: {
+            target: 'hold',
+            actions: ['addItem'],
+          },
         },
       },
+      hold: {},
     },
-    hold: {},
   },
-});
+  {
+    actions: {
+      addItem: (context, event) => {
+        context.items.push(event.item);
+        console.log(context.items);
+      },
+    },
+  }
+);
 
 const Cart = () => {
   const [state, send] = useMachine(stateMachine);
@@ -22,12 +36,17 @@ const Cart = () => {
   return (
     <div>
       <p>{state.value}</p>
+      <ul>
+        {state.context.items.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
       <button
         onClick={() => {
-          send('ADD_ITEM');
+          send('ADD_ITEM', { item: `item${Date.now()}` });
         }}
       >
-        Add Item-
+        Add Item
       </button>
     </div>
   );
