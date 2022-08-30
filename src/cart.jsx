c4227e67efeb3a3f5f1aace1e6b599afc32e1294
Sungho Kim <shirenbeat@gmail.com>
2022-08-30 17:06:58 +0900

    예제 wip

--- src/cart.jsx
+++ src/cart.jsx
@@ -1,5 +1,12 @@
-import { createMachine } from 'xstate';
+import { createMachine, assign } from 'xstate';
 import { useMachine } from '@xstate/react';
+
+const postPurchase = (cart: string[]): Promise<boolean> =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res(true);
+    }, 2000);
+  });
 
 const stateMachine = createMachine(
   {
@@ -18,20 +25,56 @@ const stateMachine = createMachine(
         },
       },
       hold: {
+        always: {
+          target: 'empty',
+          cond: 'isEmpty',
+        },
         on: {
           ADD_ITEM: {
             actions: ['addItem'],
           },
+          RESET_ITEMS: {
+            target: 'empty',
+            actions: ['resetItems'],
+          },
+          REMOVE_ITEM: {
+            actions: ['removeItem'],
+          },
+          PURCHASE: {
+            target: 'purchasing',
+          },
         },
+      },
+      purchasing: {
+        invoke: {
+          id: 'purchasing',
+          src: (context) => postPurchase(context.items),
+          onDone: {
+            target: 'done',
+            actions: ['purchased'],
+          },
+        },
+      },
+      done: {
+        type: 'final',
+        entry: ['resetItems'],
       },
     },
   },
   {
     actions: {
-      addItem: (context, event) => {
-        context.items.push(event.item);
-        console.log(context.items);
-      },
+      addItem: assign({
+        items: ({ items }, event) => [...items, event.item],
+      }),
+      resetItems: assign({
+        items: [],
+      }),
+      removeItem: assign({
+        items: ({ items }, event) => items.filter((item) => item !== event.name),
+      }),
+    },
+    guards: {
+      isEmpty: ({ items }) => items.length === 0,
     },
   }
 );
@@ -44,7 +87,10 @@ const Cart = () => {
       <p>{state.value}</p>
       <ul>
         {state.context.items.map((name, index) => (
-          <li key={index}>{name}</li>
+          <li key={index}>
+            {name}
+            <button onClick={() => send('REMOVE_ITEM', { name })}>X</button>
+          </li>
         ))}
       </ul>
       <button
@@ -53,6 +99,20 @@ const Cart = () => {
         }}
       >
         Add Item
+      </button>
+      <button
+        onClick={() => {
+          send('RESET_ITEMS');
+        }}
+      >
+        Reset Item
+      </button>
+      <button
+        onClick={() => {
+          send('PURCHASE');
+        }}
+      >
+        Purchase Items
       </button>
     </div>
   );
